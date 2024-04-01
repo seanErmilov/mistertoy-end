aa463b49f8f05728d81f4e565a6f50012e62586a
vyaron <vyaron@gmail.com>
2024-04-01 21:10:30 +0300

    removed useNewUrlParser from dbService

--- services/db.service.js
+++ services/db.service.js
@@ -22,7 +22,8 @@ async function getCollection(collectionName) {
 async function _connect() {
     if (dbConn) return dbConn
     try {
-        const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+        const client = await MongoClient.connect(config.dbURL, { useUnifiedTopology: true })
+        // const client = await MongoClient.connect(config.dbURL)
         const db = client.db(config.dbName)
         dbConn = db
         return db
